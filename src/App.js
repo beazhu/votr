624579652c4641ebbd35b7a4cc72965a9e88e9e8
beazhu <beatricezhu1231@gmail.com>
2018-09-21 22:46:57 -0700

    added a modal for new polls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 import React, { Component } from 'react';
+import { Modal, Button } from "react-bootstrap";
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showNewPoll: false,
+      savedPolls: [],
+      currentPoll: {title: "", option1: "", option2: ""},
+    };
+
+    this.showNewPoll = this.showNewPoll.bind(this);
+    this.closeNewPoll = this.closeNewPoll.bind(this);
+    this.savePoll = this.savePoll.bind(this);
+    this.handleTitleInput = this.handleTitleInput.bind(this);
+
+  }
+
+  showNewPoll() {
+    this.setState({showNewPoll: true});
+
+  }
+
+  closeNewPoll () {
+    this.setState({showNewPoll: false});
+  }
+
+  savePoll() {
+      console.log("saved",this.state.savedPolls);
+
+      var updateSaved = this.state.savedPolls;
+      updateSaved.push(this.state.currentPoll);
+      console.log("curr", updateSaved);
+      this.setState({
+        savedPolls: updateSaved,
+        currentPoll: {title: "", option1: "", option2: ""}});
+
+
+  }
+
+  handleTitleInput(e) {
+    console.log(this.state.currentPoll);
+    var poll = this.state.currentPoll;
+    poll.title = e.target.value;
+    this.setState({currentPoll: poll});
+
+  }
   render() {
     return (
       <div className="App">
@@ -12,9 +60,34 @@ class App extends Component {
         </header>
         <br/>
         <div>   
-        <input/> <button> Add Option </button>
+        <button onClick={this.showNewPoll}> Create Poll </button>
         <br/>
         <button> Confirm </button>
+        <div> 
+          {this.state.savedPolls.map((polls) => (
+            <div> {polls.title} </div>
+            ))}
+        </div>
+
+
+        <div class="newPoll-container">
+              <Modal show={this.state.showNewPoll} onHide={this.closeNewPoll} bsSize="lg">
+                <Modal.Header closeButton>
+                <Modal.Title> New Poll </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                 Title <input onChange={this.handleTitleInput} /> <br/>
+                 Option 1 <input/> <br/>
+                 Option 2 <input/> <br/>
+                </Modal.Body>
+
+                <Modal.Footer> 
+                  <Button onClick={this.savePoll}>Save</Button>
+                </Modal.Footer>
+              </Modal>
+
+              </div>
+
         
         </div>
       </div>
