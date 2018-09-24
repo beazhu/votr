a5b665c16f96ab2268a88081e766f542aa348565
beazhu <beatricezhu1231@gmail.com>
2018-09-24 16:50:04 -0700

    added firebase and functionality to submit and retrieve to firebase

--- src/App.js
+++ src/App.js
@@ -3,16 +3,102 @@ import { Modal, Button } from "react-bootstrap";
 
 import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase.js';
 
 
 function Poll (props) {
   return (
     <div>
-      {props.index}. <b>{props.poll.title}</b>
+      {props.index}. <b>{props.poll.title}</b> {props.poll.options}
 
     </div>
   );
 
+}
+
+class NewPoll extends Component {
+
+  constructor(props) {
+     super(props);
+
+     this.state = {
+       showNewPoll : false,
+       currentPoll: {title: "", options: ["",""] },
+
+     }
+
+     this.handleTitleInput = this.handleTitleInput.bind(this);
+     this.handleOptionInput = this.handleOptionInput.bind(this);
+     this.showNewPoll = this.showNewPoll.bind(this);
+     this.closeNewPoll = this.closeNewPoll.bind(this);
+     this.savePoll = this.savePoll.bind(this);
+  }
+
+  showNewPoll() {
+    this.setState({showNewPoll: true});
+
+  }
+
+  closeNewPoll () {
+    this.setState({showNewPoll: false});
+  }
+
+  handleTitleInput(e) {
+    console.log(this.state.currentPoll);
+    var poll = this.state.currentPoll;
+    poll.title = e.target.value;
+    this.setState({currentPoll: poll});
+  }
+
+  handleOptionInput(e,index) {
+    var poll = this.state.currentPoll;
+    poll.options[index] = e.target.value;
+    this.setState({currentPoll: poll});
+  }
+
+  savePoll() {
+
+      const pollsRef = firebase.database().ref('polls');
+      const item = {
+        title: this.state.currentPoll.title,
+        options: this.state.currentPoll.options
+      }
+      pollsRef.push(item);
+
+      this.setState({
+        currentPoll: {title: "", options:["",""]}
+      });
+
+
+      this.closeNewPoll();
+
+
+  }
+  render() {
+    return (
+      <div>
+      <button onClick={this.showNewPoll}> Create Poll </button>
+      <Modal show={this.state.showNewPoll} onHide={this.closeNewPoll} bsSize="lg">
+        <Modal.Header closeButton>
+        <Modal.Title> New Poll </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+         Title <input onChange={this.handleTitleInput} /> <br/>
+         {this.state.currentPoll.options.map((options, ind) => (
+           <div>
+           Option {ind+1} <input value={options} onChange={(e) => this.handleOptionInput(e,ind)}/>
+            </div>
+         ))}
+        </Modal.Body>
+
+        <Modal.Footer> 
+          <Button onClick={this.savePoll}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+      </div>
+
+      );
+  }
 }
 class App extends Component {
 
@@ -22,13 +108,34 @@ class App extends Component {
     this.state = {
       showNewPoll: false,
       savedPolls: [],
-      currentPoll: {title: "", option1: "", option2: ""},
+      currentPoll: {title: "", options: ["",""] },
     };
 
     this.showNewPoll = this.showNewPoll.bind(this);
     this.closeNewPoll = this.closeNewPoll.bind(this);
     this.savePoll = this.savePoll.bind(this);
     this.handleTitleInput = this.handleTitleInput.bind(this);
+    this.handleOptionInput = this.handleOptionInput.bind(this);
+
+  }
+
+  componentDidMount() {
+    var pollsRef = firebase.database().ref('polls');
+    pollsRef.on('value', (snapshot) => {
+      let polls = snapshot.val();
+      let newState = [];
+      for(let poll in polls) {
+        newState.push({
+          id: poll,
+          title: polls[poll].title,
+          options: polls[poll].options
+        });
+      }
+      this.setState({
+        savedPolls: newState
+      });
+    })
+
 
   }
 
@@ -42,14 +149,19 @@ class App extends Component {
   }
 
   savePoll() {
-      console.log("saved",this.state.savedPolls);
 
-      var updateSaved = this.state.savedPolls;
-      updateSaved.push(this.state.currentPoll);
-      console.log("curr", updateSaved);
+      const pollsRef = firebase.database().ref('polls');
+      const item = {
+        title: this.state.currentPoll.title,
+        options: this.state.currentPoll.options
+      }
+      pollsRef.push(item);
+
       this.setState({
-        savedPolls: updateSaved,
-        currentPoll: {title: "", options:[],}});
+        currentPoll: {title: "", options:["",""]}
+      });
+
+
       this.closeNewPoll();
 
 
@@ -60,20 +172,29 @@ class App extends Component {
     var poll = this.state.currentPoll;
     poll.title = e.target.value;
     this.setState({currentPoll: poll});
+  }
 
+  handleOptionInput(e) {
+    var poll = this.state.currentPoll;
+   // poll.options.push(e.target.value);
+    this.setState({currentPoll: poll});
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Votr</h1>
+          <h1 className="App-title">Votr: for voting </h1>
         </header>
         <br/>
         <div>   
-        <button onClick={this.showNewPoll}> Create Poll </button>
+        <div class="newPoll-container">
+            <NewPoll show={this.state.showNewPoll} savePoll={this.savePoll}/>
+
+              </div>
         <br/>
-        <div> 
+        <div>             
+        <h4>Saved Polls</h4> <br/>
           {this.state.savedPolls.map((polls,ind) => (
             <div>  
             <Poll index={ind+1} title={polls.title} poll={polls}/>
@@ -83,23 +204,7 @@ class App extends Component {
         </div>
 
 
-        <div class="newPoll-container">
-              <Modal show={this.state.showNewPoll} onHide={this.closeNewPoll} bsSize="lg">
-                <Modal.Header closeButton>
-                <Modal.Title> New Poll </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                 Title <input onChange={this.handleTitleInput} /> <br/>
-                 Option 1 <input/> <br/>
-                 Option 2 <input/> <br/>
-                </Modal.Body>
-
-                <Modal.Footer> 
-                  <Button onClick={this.savePoll}>Save</Button>
-                </Modal.Footer>
-              </Modal>
-
-              </div>
+ 
 
         
         </div>
